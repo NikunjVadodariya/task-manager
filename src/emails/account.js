23da90b1d714e8b3caad3c3e3afdcb46bcc6a5b0
NikunjVadodariya <nikunj.vadodariya@marutitech.com>
2021-06-16 20:03:16 +0530

    Hello

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,8 +4,8 @@ const key = "G.hBFd-OvTK-Zqxo4Eg.jkjkjk"
 mail.sendApiKey(key)
 
 mail.send({
-    "to": "nikunj.vadodariya@marutitech.com",
-    "from": "nikunj.vadodariya@marutitech.com",
+    "to": "nikunj.@jk.com",
+    "from": "nikunj.@jk.com",
     "subject": "This is subject",
     "text": "hello"
 })
